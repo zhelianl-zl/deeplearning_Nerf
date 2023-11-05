402aba2d0611dee0f57e6106dd93abc807aa0e28
Darshit Desai <36150235+darshit-desai@users.noreply.github.com>
2023-11-05 17:44:28 -0500

    first commit

---
+++ sampler.py
@@ -0,0 +1,45 @@
+import math
+from typing import List
+
+import torch
+from ray_utils import RayBundle
+from pytorch3d.renderer.cameras import CamerasBase
+
+
+# Sampler which implements stratified (uniform) point sampling along rays
+class StratifiedRaysampler(torch.nn.Module):
+    def __init__(
+        self,
+        cfg
+    ):
+        super().__init__()
+
+        self.n_pts_per_ray = cfg.n_pts_per_ray
+        self.min_depth = cfg.min_depth
+        self.max_depth = cfg.max_depth
+
+    def forward(
+        self,
+        ray_bundle,
+    ):
+        # TODO (1.4): Compute z values for self.n_pts_per_ray points uniformly sampled between [near, far]
+        z_vals = torch.linspace(self.min_depth, self.max_depth, self.n_pts_per_ray, device = "cuda")
+        # print("Z-vals shape = ", z_vals.shape)
+        # print("origines shape = ", ray_bundle.origins.reshape(-1,1,3).repeat(1, self.n_pts_per_ray, 1).shape)
+        # print("directions shape = ", ray_bundle.directions.reshape(-1,1,3).repeat(1, self.n_pts_per_ray, 1).shape)
+        # TODO (1.4): Sample points from z values
+        origins = ray_bundle.origins.reshape(-1, 1, 3).repeat(1, self.n_pts_per_ray, 1)
+        directions = ray_bundle.directions.reshape(-1, 1, 3).repeat(1, self.n_pts_per_ray, 1)
+        z_vals = z_vals.reshape(1, -1, 1).repeat(ray_bundle.directions.shape[0], 1, 1)
+        sample_points = origins + directions * z_vals 
+
+        # Return
+        return ray_bundle._replace(
+            sample_points=sample_points,
+            sample_lengths=z_vals * torch.ones_like(sample_points[..., :1]),
+        )
+
+
+sampler_dict = {
+    'stratified': StratifiedRaysampler
+}
